6ac4aa71edf77c6d596cd57647ac79adeff2dfb6
sayf20 <mosratisayf20@gmail.com>
2023-12-26 05:54:10 -0800

    correction de fichier

--- Route/R_Profile.js
+++ Route/R_Profile.js
@@ -1,39 +1,65 @@
-const { Model, DataTypes } = require("sequelize");
-const connection = require("./db");
+const { Router } = require("express");
+const Profile = require("../models/Profile");
+const checkAuth = require("../middlewares/checkAuth");
+const router = new Router();
 
-class Profile extends Model {}
-
-Profile.init(
-  {
-    idUser:{
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      References:{
-        Model: 'User',
-        key: "idUser",
-      },
-      
-  },
-    nickname:{
-        type: DataTypes.STRING,
-        unique: true,
-        allowNull: false,
-    },
-    rank: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-          isIn: [['Bronze','gold','silver', 'plat', 'champion','Diamond']],
-        },
-      },
-    },
-  {
-    sequelize: connection,
-  }
-);
-
-Profile.belongsTo(User,{
-  foreignKey: 'idUser',
+router.get("/Profile", checkAuth({ transient: true }), async (req, res, next) => {
+    try {
+        const Profiles=await Profile.findAll();
+        res.status(200).json(Profiles);
+      } catch (error) {
+        next(error);
+      }
 });
 
-module.exports = Profile;
+router.get("/Profile/:idUser", checkAuth({ transient: true }), async (req, res, next) => {
+    try {
+        const Profile1=await Profile.findbyPk(req.params.idUser);
+        if(!Profile1){
+            res.sendStatus(404);
+        }
+        res.status(200).json(Profile1);
+    } catch (error) {
+        next(error);
+    }
+});
+
+router.post("/Profile", checkAuth({ transient: true }), async (req, res, next) => {
+    try {
+        const newProfile=await Profile.create(req.body);
+        res.status(201).json(newProfile);
+      } catch (error) {
+        next(error);
+      }
+});
+
+
+router.put("/Profile/:idUser", checkAuth({ transient: true }), async (req, res, next) => {
+    try {
+        const profile =await Profile.findByPK(req.params.idUser);
+        if (!profile){
+            res.sendStatus(404);
+        }
+        await profile.update(req.body);
+        res.status(200).json(profile);
+      } catch (error) {
+        next(error);
+      }
+});
+
+router.delete("/Profile/:idUser", checkAuth({ transient: true }), async (req, res, next) => {
+    try {
+        const delProfile =await Profile.destroy({
+            where: {
+                idUser: req.params.idUser,
+            } ,
+        });
+        
+        res.sendStatus(delskin ? 204 : 404);
+      } catch (error) {
+        next(error);
+      }
+});
+
+
+module.exports = router;
